90ec738c87a343662e1262af6e61b222171abc49
Richard Russell <richardr39@gmail.com>
2018-08-26 20:16:24 +0100

    fix: remove flags enum attribute

--- Dates.Recurring/Enums/Ordinal.cs
+++ Dates.Recurring/Enums/Ordinal.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 
 namespace Dates.Recurring
 {
-    [Flags]
     public enum Ordinal : int
     {
         [Description("First")]
