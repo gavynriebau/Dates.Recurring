c00897195f0e9b550edda895c5dd1b37cc282d8c
Walt Daniels <waltdaniels@live.com>
2018-10-24 17:31:40 -0400

    Added support for skipping days (primarily weekends) to the recurring day processor

--- Dates.Recurring.Tests/DailyRecurrenceTests.cs
+++ Dates.Recurring.Tests/DailyRecurrenceTests.cs
@@ -49,5 +49,45 @@ namespace Dates.Recurring.Tests
             Assert.Equal(new DateTime(2015, 1, 7), daily.Next(new DateTime(2015, 1, 5)));
         }
 
+        [Fact]
+        public void Daily_EveryDay_ExcludeWeekends()
+        {
+            // Arrange.
+            IRecurring daily = Recurs
+                .Starting(new DateTime(2015, 1, 1))
+                .Every(1)
+                .Days()
+                .Including(Day.MONDAY | Day.TUESDAY | Day.WEDNESDAY | Day.THURSDAY | Day.FRIDAY)
+                .Build();
+
+            // Act.
+
+            // Assert.
+            Assert.Equal(new DateTime(2015, 1, 1), daily.Next(new DateTime(2014, 7, 3)));
+            Assert.Equal(new DateTime(2015, 1, 2), daily.Next(new DateTime(2015, 1, 1)));
+            Assert.Equal(new DateTime(2015, 1, 5), daily.Next(new DateTime(2015, 1, 2)));
+            Assert.Equal(new DateTime(2015, 1, 6), daily.Next(new DateTime(2015, 1, 5)));
+        }
+
+        [Fact]
+        public void Daily_EveryThirdDay_ExcludeWeekends()
+        {
+            // Arrange.
+            IRecurring daily = Recurs
+                .Starting(new DateTime(2015, 1, 1))
+                .Every(3)
+                .Days()
+                .Including(Day.MONDAY | Day.TUESDAY | Day.WEDNESDAY | Day.THURSDAY | Day.FRIDAY)
+                .Build();
+
+            // Act.
+
+            // Assert.
+            Assert.Equal(new DateTime(2015, 1, 1), daily.Next(new DateTime(2014, 7, 3)));
+            Assert.Equal(new DateTime(2015, 1, 6), daily.Next(new DateTime(2015, 1, 1)));
+            Assert.Equal(new DateTime(2015, 1, 9), daily.Next(new DateTime(2015, 1, 6)));
+            Assert.Equal(new DateTime(2015, 1, 14), daily.Next(new DateTime(2015, 1, 9)));
+        }
+
     }
 }
